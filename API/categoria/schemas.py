b2434bb84cb73a5f8d940a515f37e8d0b21bfc0d
Eric Petterson <ericpl.ff@gmail.com>
2024-05-10 20:38:44 -0300

    Atualizando os schemas

--- API/categoria/schemas.py
+++ API/categoria/schemas.py
@@ -3,7 +3,7 @@ from pydantic import UUID4, Field
 from API.contrib.schema_base import SchemaBase
 
 class Categoria(SchemaBase):
-    nome: Annotated[str, Field(description="Nome da categoria", example="Scale", min_length=3, max_length=10)]
+    nome: Annotated[str, Field(description="Nome da categoria", example="Scale", min_length=1, max_length=10)]
     
 class CategoriaOut(Categoria):
     id: Annotated[UUID4, Field(description="ID da categoria")]
